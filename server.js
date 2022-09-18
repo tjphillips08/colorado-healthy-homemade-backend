d62658a8a13d5fb594148d509c8caf3c1ff9026b
maclamour <maclamour@gmail.com>
2022-09-18 13:07:36 -0400

    setting up server js

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+require('./config/db.connection')
+
+const express = require ('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const {PORT= 4000} = process.env
