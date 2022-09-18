b60b4c6616935b28980f88e77170dfd7553216d8
maclamour <maclamour@gmail.com>
2022-09-18 15:33:37 -0400

    get and post routes both work

--- controllers/food-controller.js
+++ controllers/food-controller.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const {Food} = require('../models')
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
 // FOOD INDEX ROUTE
 router.get('/',async (req, res) => {
-    res.status(200).json({message:"food index route"})
+    try{
+        const food = await Food.find({})
+        res.json(food)
+    } catch(err) {
+        res.status(400).json(err)
+    }
 })
-
 // FOOD CREATE ROUTE
-router.post('/',async (req, res) => {
-    res.status(200).json({message:"food create route"})
-})
+router.post("/", async (req, res) => {
+    try {
+      // create new person
+      res.json(await Food.create(req.body));
+    } catch (error) {
+      //send error
+  
+      res.status(400).json(error);
+    }
+  });
 
 
 module.exports = router
