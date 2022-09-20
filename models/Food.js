b95c9cbdd394b4886053a4e1fa1b74c4c4a3a5bb
maclamour <maclamour@gmail.com>
2022-09-20 08:17:15 -0400

    updated schema

--- models/Food.js
+++ models/Food.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const FoodSchema = new mongoose.Schema(
     { 
     name:{type:{type:String, required:true},  
-    type: {type:String, required:true},
+    desc: {type:String, required:true},
     day:{type:String, required:true}, 
     image: {type:String, required:true},  
-    cuisine: {type:String, required:true},  
+    portions: {type:Number, required:true},  
+    created: {type:Date, required:true, default:Date.now},
+
+    }
 
     },
     
