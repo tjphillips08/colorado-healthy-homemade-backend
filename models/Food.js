349d678b13094a88db9baa91f8b3076f314778eb
maclamour <maclamour@gmail.com>
2022-09-20 07:56:23 -0400

    updating food schema and dotenv

--- models/Food.js
+++ models/Food.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
-const FoodSchema = new mongoose.Schema({ 
-    name:String,
-    type: String,
-    image: String,
-    cuisine: String,
+const FoodSchema = new mongoose.Schema(
+    { 
+    name:{type:{type:String, required:true},  
+    type: {type:String, required:true},
+    day:{type:String, required:true}, 
+    image: {type:String, required:true},  
+    cuisine: {type:String, required:true},  
 
-    }, {timestamps: true});
+    },
+    
+    {timestamps: true}
+    
+    );
 
     const Food = mongoose.model('Food', FoodSchema);
 
